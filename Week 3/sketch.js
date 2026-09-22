463af83816bef7f8b499ecf9014b8b3dba981685
Farbas27 <farhaanbasdeo27@gmail.com>
2026-09-22 12:38:04 +0100

    week3 4 punten

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -10,27 +10,73 @@ let actieveSpeler;
 let spelActief = true
 let winnaar = null;
 let w, h;
+let clickGeluid;
+let winLijn = null;
+
+function preload(){
+    soundFormats('mp3');
+    clickGeluid = loadSound('matthewvakaliuk73627-mouse-click-290204.mp3');
+}
 
 function setup() {
     createCanvas(400, 450);
+    clickGeluid.setVolume(0.3);
     w = width / 3;
     h = (height - 50) / 3;
     resetSpel();
 }
 
 function draw() {
-    background(255);
+    bepaalAchtergrond();
+    tekenHoverEffect();
     tekenRaster();
     tekenZetten();
+    tekenWinLijn();
     tekenStatus();
 }
 
+// veranderd achtergrond kleur
+function bepaalAchtergrond() {
+    if (spelActief) {
+        if (actieveSpeler === spelers[0]) {
+            background(240, 245, 255);
+        } else {
+            background(255, 240, 240)
+        }
+    } else {
+        if (winnaar === 'Gelijkspel') {
+            background(230);
+        } else if (winnaar === spelers[0]) {
+            background(200, 220, 255);
+        } else {
+            background(255, 200, 200);
+        }
+    }
+}
+
+// hover effect
+function tekenHoverEffect() {
+    if (!spelActief) return;
+
+    let j = floor(mouseX / w);
+    let i = floor(mouseY / h);
+
+    if (i >= 0 && i < 3 && j >= 0 && j < 3 && board[i][j] === '') {
+        noStroke();
+        if (actieveSpeler === spelers[0]) {
+            fill(0, 0, 255, 30);
+        } else {
+            fill(255, 0, 0, 30);
+        }
+        rect(j * w, i * h, w, h);
+    }
+}
 // teken raster
 function tekenRaster() {
-    stroke(0);
+    stroke(40);
     strokeWeight(4);
     for (let i = 1; i < 3; i++) {
-            line(i * w, 0, i * w, height - 50);
+        line(i * w, 0, i * w, height - 50);
         line(0, i * h, width, i * h);
     }
 }
@@ -60,6 +106,32 @@ function tekenZetten() {
     }
 }
 
+// teken de win streep
+function tekenWinLijn() {
+    if (spelActief || !winLijn || winnaar === 'Gelijkspel') return;
+
+    strokeWeight(8);
+    stroke(winnaar === spelers[0] ? color(0, 0, 255) : color(255, 0, 0));
+
+    if (winLijn.type === 'rij') {
+        let y = winLijn.index * h + h / 2;
+        line(20, y, width - 20, y);
+    }
+
+    if (winLijn.type === 'kolom') {
+        let x = winLijn.index * w + w / 2;
+        line(x, 20, x, height - 70);
+    }
+
+    if (winLijn.type === 'diagonaal1') {
+        line(20, 20, width - 20, height - 70);
+    }
+
+    if (winLijn.type === 'diagonaal2') {
+        line(width - 20, 20, 20, height - 70);
+    }
+}
+
 // toon status
 function tekenStatus() {
     textAlign(CENTER, CENTER);
@@ -68,11 +140,11 @@ function tekenStatus() {
     fill(0);
 
     if (spelActief) {
-       text(
-    "Beurt: " + (actieveSpeler === spelers[0] ? "Speler 1 (Blauw)" : "Speler 2 (Rood)"),
-    width / 2,
-    height - 25
-);
+        text(
+            "Beurt: " + (actieveSpeler === spelers[0] ? "Speler 1 (Blauw)" : "Speler 2 (Rood)"),
+            width / 2,
+            height - 25
+        );
     } else {
         if (winnaar === 'Gelijkspel') {
             text("Het is een gelijkspel Klik om te herstarten.", width / 2, height - 25);
@@ -91,6 +163,8 @@ function mousePressed() {
 
         if (i >= 0 && i < 3 && j >= 0 && j < 3) {
             if (board[i][j] === '') {
+                clickGeluid.stop();
+                clickGeluid.play();
                 board[i][j] = actieveSpeler;
                 controleerEindeSpel();
                 if (spelActief) {
@@ -109,44 +183,54 @@ function wisselSpeler() {
 
 function resetSpel() {
     board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
 
     actieveSpeler = spelers[0];
     spelActief = true;
     winnaar = null;
+    winLijn = null;
 }
 
 function controleerEindeSpel() {
     // horizontaal
     for (let i = 0; i < 3; i++) {
-        if (board[i][0] !== '' && 
-            board[i][0] === board[i][1] && 
+        if (board[i][0] !== '' &&
+            board[i][0] === board[i][1] &&
             board[i][1] === board[i][2]) {
+
+                   winLijn = { type: 'rij', index: i };
             kondigWinnaar(board[i][0]);
             return;
         }
     }
     // verticaal
-    for (let j = 0; j < 3; j++)
-    if (board[0][j] !== '' && 
-        board[0][j] === board[1][j] && 
-        board[1][j] === board[2][j]) {
-        kondigWinnaar([0][j]);
-        return;
+    for (let j = 0; j < 3; j++) {
+        if (board[0][j] !== '' &&
+            board[0][j] === board[1][j] &&
+            board[1][j] === board[2][j]) {
+
+                winLijn = { type: 'kolom', index: j };
+            kondigWinnaar(board[0][j]);
+            return;
+        }
     }
-        // controleer diagonaal
-    if (board[0][0] !== '' && 
-        board[0][0] === board[1][1] && 
+    // controleer diagonaal
+    if (board[0][0] !== '' &&
+        board[0][0] === board[1][1] &&
         board[1][1] === board[2][2]) {
+
+            winLijn = { type: 'diagonaal1' };
         kondigWinnaar(board[0][0]);
         return;
     }
-    if (board[0][2] !== '' && 
+    if (board[0][2] !== '' &&
         board[0][2] === board[1][1] &&
         board[1][1] === board[2][0]) {
+
+           winLijn = { type: 'diagonaal2' };
         kondigWinnaar(board[0][2]);
         return;
     }
@@ -161,7 +245,7 @@ function controleerEindeSpel() {
     }
     if (legeCellen === 0) {
         winnaar = 'Gelijkspel';
-spelActief = false;
+        spelActief = false;
     }
 }
 function kondigWinnaar(s) {
